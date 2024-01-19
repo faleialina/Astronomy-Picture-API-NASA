6936a573564fd87765b96b04f859269c1e34c6e7
a.falei <august3@mail.ru>
2024-01-20 01:50:24 +0300

    Astronomical picture with date selection

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,23 +1,43 @@
 'use client'
-import axios from 'axios'
+import axios from 'axios';
 import { useEffect, useState } from 'react';
-import styles from './page.module.css'
+import styles from './page.module.css';
 
 
 export default function Home() {
   const [obj, setObj] = useState('');
+  const [objData, setObjData] = useState('');
+  const [startDate, setStartDate] = useState('');
+
   async function send() {
     const res = await axios.get('https://api.nasa.gov/planetary/apod?api_key=5U0ftZibSq2uZy1Alv9otc7dKS5lOYrlaD46IwPv');
     setObj(res.data)
-  }
+  };
+  async function sendData() {
+    const res = await axios.get(`https://api.nasa.gov/planetary/apod?date=${startDate}&api_key=5U0ftZibSq2uZy1Alv9otc7dKS5lOYrlaD46IwPv`);
+    setObjData(res.data)
+  };
   useEffect(() => {
     send()
   }, []);
+  useEffect(() => {
+    sendData()
+    console.log(objData);
+  }, []);
+
   return (
     <main className={styles.main}>
-      <img src={obj?.url} width={800}
-        alt="Picture" />
+      <img src={obj?.url} width={800} alt="Picture" />
       <div>{obj?.url}</div>
+
+      <div className={styles.wrap}>
+        <input className={styles.input} type="date" id="start" name="trip-start" onChange={(e) => { setStartDate(e.target.value) }} />
+        <button className={styles.button} onClick={sendData}>Click</button>
+      </div>
+
+      <img src={objData?.url} width={800} alt="Picture" />
+      <div>{objData?.url}</div>
+
     </main>
   )
-}
+};
